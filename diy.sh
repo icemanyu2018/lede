11f1e092d1b50435e344c50472254af8da3ba58b
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-15 17:10:14 +0800

    Create diy.sh to customize system settings
    This script modifies various default settings for the system, including LAN IP, root password, default shell, timezone, theme, and WiFi SSID.

---
+++ diy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. 修改默认 LAN 口 IP 为 192.168.6.1
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# 2. 设置 root 默认密码为 123456789
+sed -i 's/root:::0:99999:7:::/root:$1$wE47n1$yv2Kq5m7qIe3oR5v1hGjH\/:19000:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 3. 将默认登录 Shell 改为 zsh
+sed -i 's/\/bin\/ash/\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+# 4. 设置默认时区为上海 (CST-8) 并锁定中文
+sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/set system.@system\[-1\].timezone='CST-8'/a \\\t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+sed -i 's/auto/zh_cn/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 5. 设置默认主题为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 6. 自定义默认 WiFi SSID (ImmortalWrt-2.4G / ImmortalWrt-5G) 且默认无密码
+sed -i 's/OpenWrt/ImmortalWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt-2.4/ImmortalWrt-2.4G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt-5.8/ImmortalWrt-5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh

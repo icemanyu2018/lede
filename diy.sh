93a98fa146e4e9570570603e370161bc1d6a6820
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-16 09:44:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -17,13 +17,23 @@ sed -i 's/auto/zh_cn/g' feeds/luci/modules/luci-base/root/etc/config/luci
 # 5. 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 6. 自定义默认 WiFi SSID (ImmortalWrt-2.4G / ImmortalWrt-5G) 且默认无密码
+# 6. 自定义默认 WiFi SSID 为 ImmortalWrt-2.4G / 5G
 sed -i 's/OpenWrt/ImmortalWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/ImmortalWrt-2.4/ImmortalWrt-2.4G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/ImmortalWrt-5.8/ImmortalWrt-5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 7. 拉取单包插件到 package 目录
-rm -rf package/daed package/passwall2 package/pushbot
-git clone --depth 1 https://github.com/wkccd/luci-app-daed-runfiles.git package/daed
+# 7. 清理 feeds 中可能引发冲突的同名包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-openclash
+
+# 8. 直接拉取所需的所有独立插件到 package 目录
+rm -rf package/passwall2 package/passwall_packages package/mosdns package/v2ray-geodata package/openclash package/daed package/pushbot
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash
+git clone --depth 1 https://github.com/wkccd/luci-app-daed-runfiles.git package/daed
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot.git package/pushbot
